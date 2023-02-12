141010c25d6e34d6b954b627a8ee8d34ba3c056c
Akash Khurana <akashkhurana28@gmail.com>
2023-02-12 15:43:31 +0530

    Adding authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Group1 from './img/logo.png';
 import Avatar from './img/avatar.png';
-import {MdShoppingBasket} from 'react-icons/md';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {app} from '../firebase.config';
+import { MdShoppingBasket } from 'react-icons/md';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+
+
 const Header = () => {
+  const firebaseAuth  = getAuth(app);
+  const provider = new GoogleAuthProvider(); 
+  const login  = async () =>{
+  const response = await signInWithPopup(firebaseAuth,provider);
+  console.log(response);
+  }
+
+
   return (
     <header className="fixer z-50 w-screen p-6 px-16 ">
       {/* desktop & tablet */}
@@ -14,21 +26,26 @@ const Header = () => {
           <p className="text-headingColor text-x1 font-semibold">Eat 24</p>
         </Link>
         <div className='flex items-center gap-8'>
-        <ul className='flex items-center gap-8 '>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Home</li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Menu</li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">About Us</li>
-          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Service</li>
-        </ul>
-        <div className='relative flex items-center justify-center'>
-        <MdShoppingBasket className='text-textColor text-2xl cursor-pointer'/ >
-          <div className=' absolute -top-3.5 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
-            <p className='text-xs text-white font-semibold'>2</p>
+          <ul className='flex items-center gap-8 '>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Home</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Menu</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">About Us</li>
+            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out">Service</li>
+          </ul>
+          <div className='relative flex items-center justify-center'>
+            <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+            <div className=' absolute -top-3.5 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+              <p className='text-xs text-white font-semibold'>2</p>
+            </div>
           </div>
-        </div>
-        
-      <motion.img whileTap={{scale:0.6}} src={Avatar} className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"  alt='Profile'/>
-
+          <div className="relative">
+            <motion.img 
+            whileTap={{ scale: 0.6 }} 
+            src={Avatar} 
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer" 
+            alt='Profile' 
+            onClick={login}/>
+          </div>
 
         </div>
       </div>
