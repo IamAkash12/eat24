bc001db7753bc10f0c15e7800998c861e6351a94
Akash Khurana <akashkhurana28@gmail.com>
2023-02-13 23:49:39 +0530

    Adding dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md';
 import Group1 from './img/logo.png';
 import Avatar from './img/avatar.png';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import {app} from '../firebase.config';
-import { MdShoppingBasket } from 'react-icons/md';
+import { app } from '../firebase.config';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { actionType } from '../context/reducer';
@@ -11,19 +11,26 @@ import { useStateValue } from '../context/StateProvider';
 
 
 const Header = () => {
-  const firebaseAuth  = getAuth(app);
-  const provider = new GoogleAuthProvider(); 
-const[{user},dispatch]= useStateValue()
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const [{ user }, dispatch] = useStateValue()
 
-  const login  = async () =>{
-    const {
-      user: {refreshtoken, providerData }
-    }= await signInWithPopup(firebaseAuth,provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0],
-    });
-    localStorage.setItem('user',JSON.stringify(providerData[0]));
+  const [isMenu,setIsMenu] = useState(false);
+
+  const login = async () => {
+    if (!user) {
+      const {
+        user: { refreshtoken, providerData }
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      localStorage.setItem('user', JSON.stringify(providerData[0]));
+    }
+    else{
+      setIsMenu(!isMenu);
+    }
   };
 
 
@@ -49,14 +56,31 @@ const[{user},dispatch]= useStateValue()
             </div>
           </div>
           <div className="relative">
-            <motion.img 
-            whileTap={{ scale: 0.6 }} 
-            src={user? user.photoURL : Avatar} 
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full" 
-            alt='Profile' 
-            onClick={login}/>
-          </div>
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={user ? user.photoURL : Avatar}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt='Profile'
+              onClick={login} />
 
+            {
+              isMenu &&(
+                <motion.div initial={{opacity:0,scale:0.6}}
+                animate={{opacity:1,scale:1}}
+                exit={{opacity:0,scale:0.6}}
+                className='w-40 bg-primary shadow-xl rounded-lg flex flex-col absolute top-12 right-0 '>
+              {
+                user && user.email === "akashkhurana28@gmail.com" && (
+                  <Link to={'/createItem'}>
+                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> New Item <MdAdd /></p>
+                  </Link>
+                )
+              }
+              <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'> Log out  <MdLogout /></p>
+            </motion.div>
+              )
+            }
+          </div>
         </div>
       </div>
 
